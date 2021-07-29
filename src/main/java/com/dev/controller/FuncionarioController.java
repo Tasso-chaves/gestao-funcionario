74ae317ed747206d083fd705860d6cc52824616c
Tasso-chaves <tassochav@gmail.com>
2021-07-28 21:30:52 -0300

    implementado exclusão e alteração

--- src/main/java/com/dev/controller/FuncionarioController.java
+++ src/main/java/com/dev/controller/FuncionarioController.java
@@ -1,14 +1,19 @@
 package com.dev.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.dev.model.Funcionario;
 import com.dev.repository.FuncionarioRepository;
+
+import ch.qos.logback.core.helpers.ThrowableToStringArray;
 
 @Controller
 public class FuncionarioController {
@@ -34,6 +39,28 @@ public class FuncionarioController {
 		
 		return "redirect:/rh/funcionarios";
 		
+	}
+	
+	@GetMapping("rh/funcionarios/{id}")
+	public String alterarFuncionario(@PathVariable("id") Long id, Model model) {
+		Optional<Funcionario> funcionarioOpt = funcionarioRepository.findById(id);
+		if(funcionarioOpt.isEmpty()) {
+			throw new IllegalArgumentException("Funcionario Invalido");
+		}
+		
+		model.addAttribute("funcionario", funcionarioOpt.get());
+		return "rh/funcionarios/formulario";
+	}
+	
+	@GetMapping("/rh/funcionarios/excluir/{id}")
+	public String excluirFuncionario(@PathVariable("id") Long id) {
+		Optional<Funcionario> funcionarioOpt = funcionarioRepository.findById(id);
+		if(funcionarioOpt.isEmpty()) {
+			throw new IllegalArgumentException("Funcionario Invalido");
+		}
+		
+		funcionarioRepository.delete(funcionarioOpt.get());
+		return "redirect:/rh/funcionarios";
 	}
 
 }
